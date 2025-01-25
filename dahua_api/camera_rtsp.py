9e08a2dc745e3f059fbc9ac55c7b7476e32a07dd
Dmitriy Rychapov <deems23@gmail.com>
2025-01-25 19:23:39 +0200

    Update camera_rtsp.py

--- dahua_api/camera_rtsp.py
+++ dahua_api/camera_rtsp.py
@@ -2,18 +2,19 @@
 
 from .camera_common import dahua_get, dahua_set
 
-def get_rtsp_config(ip, user, pwd):
+def get_rtsp_config(ip, user, pwd, timeout=5):
     """
-    Пример: cgi-bin/configManager.cgi?action=getConfig&name=RTSP
+    GET RTSP
+    cgi-bin/configManager.cgi?action=getConfig&name=RTSP
     """
     cgi = "cgi-bin/configManager.cgi?action=getConfig&name=RTSP"
-    return dahua_get(ip, user, pwd, cgi)
+    return dahua_get(ip, user, pwd, cgi, timeout=timeout)
 
-def set_rtsp_config(ip, user, pwd, rtsp_port=554, rtsp_auth="digest"):
+def set_rtsp_config(ip, user, pwd, rtsp_port=554, rtsp_auth="digest", timeout=5):
     """
-    Пример:
+    SET RTSP
       RTSP.ProtoPort=554
-      RTSP.AuthType=digest (или basic, disable)
+      RTSP.AuthType=digest / basic / disable
     """
     base = "cgi-bin/configManager.cgi?action=setConfig"
     cgi = (
@@ -21,4 +22,4 @@ def set_rtsp_config(ip, user, pwd, rtsp_port=554, rtsp_auth="digest"):
         f"&RTSP.ProtoPort={rtsp_port}"
         f"&RTSP.AuthType={rtsp_auth}"
     )
-    return dahua_set(ip, user, pwd, cgi)
+    return dahua_set(ip, user, pwd, cgi, timeout=timeout)
